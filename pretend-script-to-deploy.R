f0cc2a2d7d4632f407a369d4ae670acaec5fe017
charliejhadley <charliejhadley@gmail.com>
2024-07-05 13:27:19 +0100

    add pretend script

--- pretend-script-to-deploy.R
+++ pretend-script-to-deploy.R
@@ -1,4 +1,13 @@
 library(shiny)
 
 
-deployApp()
+ui <- fluidPage(
+  h1("Proper title"),
+  h6("foo"),
+  p("Para 1", p("para2"))
+)
+
+server <- function(input, output, session){}
+
+
+shinyApp(ui, server)
